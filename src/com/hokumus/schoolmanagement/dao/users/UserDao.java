28c70e042d0998e9b4a2c68f074072568855def7
vektorel <vektorel@lab7-ogr>
2020-08-22 11:47:55 +0300

    user pojo added hibernate cfg mapping and runner added userdao added baseentity updated

---
+++ src/com/hokumus/schoolmanagement/dao/users/UserDao.java
@@ -0,0 +1,16 @@
+package com.hokumus.schoolmanagement.dao.users;
+
+import java.util.List;
+
+import com.hokumus.schoolmanagement.dao.util.DBServices;
+import com.hokumus.schoolmanagement.model.user.Users;
+
+public class UserDao extends DBServices<Users>{
+	
+	@Override
+	public List<Users> getir(String kolonAdi, String deger, Users temp) {
+		// TODO Auto-generated method stub
+		return super.getir(kolonAdi, deger, temp);
+	}
+
+}
